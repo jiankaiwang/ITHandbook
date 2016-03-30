9b609945b754b8d717f2d0e241888c2891ad5506
Jian-Kai Wang <s0973078@alumni.ncyu.edu.tw>
2016-03-30 22:52:14 +0800

    Update js/general.js

--- js/general.js
+++ js/general.js
@@ -1,9 +1,11 @@
 // add content topic link
-var allH3 = $("h3");
-$('.normal h1').after('<div id="listItem" style="clear: both; padding: 1px"></div>');
-$('#listItem').append('<h4>主題選單</h4><ul>');
-for(var i = 0 ; i < allH3.length ; i++) {
-  $(allH3[i]).before('<a name="' + 'h3_' + i + '"></a>');
-  $('#listItem').append('<h5><li><a style="clear:both; font-size: 16px;" href="#' + 'h3_' + i + '">' + $(allH3[i]).text() + '</a></li></h5>');
-}
-$('#listItem').append('</ul>');
+$(function() {
+  var allH3 = $("h3");
+  $('.normal h1').after('<div id="listItem" style="clear: both; padding: 1px"></div>');
+  $('#listItem').append('<h4>主題選單</h4><ul>');
+  for(var i = 0 ; i < allH3.length ; i++) {
+    $(allH3[i]).before('<a name="' + 'h3_' + i + '"></a>');
+    $('#listItem').append('<h5><li><a style="clear:both; font-size: 16px;" href="#' + 'h3_' + i + '">' + $(allH3[i]).text() + '</a></li></h5>');
+  }
+  $('#listItem').append('</ul>');
+});
